21f45583a43a7599d88d4ff297c22958216302d5
Deo-Siatah <deosiatah0@gmail.com>
2025-02-26 11:43:28 +0300

    Add awesome new feature

---
+++ trial1.py
@@ -0,0 +1,13 @@
+#import time 
+#for x in reversed(range(10)):
+#    time.sleep(1)
+#    print(x)
+#print("Happy birthday")
+
+# def myself(name):
+#     print(f"I am {name}")
+# myself("deo")
+
+import pygame
+alarm_time = input("Enter alarm time(00:00:0):")
+
